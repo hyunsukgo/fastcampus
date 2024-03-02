b96141b07311bf83abdbf80d6c378219eb8a730a
hyunseok-pc <hyunsukgo@naver.com>
2024-03-03 01:05:54 +0900

    init

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region = "ap-northeast-2"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "fastcampus-backend"
+    key = "tfstate/fastcampus"
+    region = "us-east-1"
+  }
+}
