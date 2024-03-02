a6cec538e64c1257f2cab66f9d57713bfd9bde90
hyunseok-pc <hyunsukgo@naver.com>
2024-03-02 22:43:05 +0900

    init

---
+++ nat.tf
@@ -0,0 +1,17 @@
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.comento-public-c.id
+
+  tags = {
+    Name = "Comento-NAT"
+    Env = "Comento"
+    Created = "20230619"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.gw]
+}
+
+resource "aws_eip" "nat" {
+}
