ce4399681d2c460928e8bdb8c774f3e1787c7353
svcadm <86656639+svcadm@users.noreply.github.com>
2024-03-20 22:48:09 +0900

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -3,8 +3,8 @@ resource "aws_vpc" "vpc" {
   enable_dns_hostnames = true
   enable_dns_support   = true
   instance_tenancy     = "default"
-
   tags = {
     Name = "fastcampus VPC"
+    Service = "Day1Company"
   }
 }
